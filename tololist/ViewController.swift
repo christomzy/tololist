a6ebd0a8a14f614a48d23873ed02a45329d7ddb2
Crispin Ganew <crispinganew@crispins-macbook-pro.local>
2018-11-29 19:58:37 +0100

    Initial Commit

---
+++ tololist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tololist
+//
+//  Created by Crispin Ganew on 2018-11-29.
+//  Copyright © 2018 com.christ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
